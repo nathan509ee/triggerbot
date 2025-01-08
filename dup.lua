01f2a003b6e34c353f11a0dbb8726c8a1f4a1656
nathan509ee <maximiano.nathan12@gmail.com>
2025-01-08 03:36:57 -0300

    Update dup.lua

--- dup.lua
+++ dup.lua
@@ -1,26 +1,21 @@
 -- Obtém o jogador
 local player = game.Players.LocalPlayer
 
--- A função para prevenir a morte
-local function preventDeath(character)
+-- Função para prevenir a perda de vida
+local function preventHealthLoss(character)
     -- Aguardar até que o personagem seja carregado
     if character == nil then return end
 
-    -- Desativar os danos que poderiam matar o personagem
+    -- Desativar os danos que poderiam diminuir a vida do personagem
     local humanoide = character:WaitForChild("Humanoid")
-    
-    -- Previne a morte do personagem
-    humanoide.Died:Connect(function()
-        humanoide.Health = humanoide.MaxHealth -- Sempre mantém o personagem com a vida cheia
-    end)
-    
-    -- Previne qualquer dano ao personagem
-    humanoide.HealthChanged:Connect(function()
+
+    -- Previne que a saúde diminua
+    humanoide.HealthChanged:Connect(function(health)
         if humanoide.Health < humanoide.MaxHealth then
-            humanoide.Health = humanoide.MaxHealth -- Restaura a saúde automaticamente
+            humanoide.Health = humanoide.MaxHealth -- Restaura a saúde automaticamente para o máximo
         end
     end)
 end
 
 -- Quando o personagem do jogador for carregado, aplicamos a prevenção
-player.CharacterAdded:Connect(preventDeath)
+player.CharacterAdded:Connect(preventHealthLoss)
